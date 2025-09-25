979fa18774bdd692247037e1289240a6571d25e6
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-09-25 20:43:06 +0200

    Dodate osnovne rute i axios konfiguracija

--- gozba-na-klik/src/App.jsx
+++ gozba-na-klik/src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Welcome from "./pages/Welcome";
+import Admin from "./pages/Admin";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin/>} />
+      </Routes>
+    </Router>
+  );
+}
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default App;
--- gozba-na-klik/src/index.jsx
+++ gozba-na-klik/src/index.jsx
@@ -2,4 +2,10 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-createRoot(document.getElementById('root')).render(<App/>)
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+)
