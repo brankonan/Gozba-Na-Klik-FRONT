642622e6a031697f172093b1b90349fed3a14761
Vula <vvulic707@gmail.com>
2025-09-26 20:23:43 +0200

    Welcome page,register,login

--- gozba-na-klik/src/App.jsx
+++ gozba-na-klik/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
--- gozba-na-klik/src/api/axios.ts
+++ gozba-na-klik/src/api/axios.ts
@@ -1,1 +1,19 @@
 
+// src/api/axios.ts
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api",
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;   // 👈 OVO dodaj
