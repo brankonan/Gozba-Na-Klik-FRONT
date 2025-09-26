53eedfb951b8d97639a2e65e357c6528b8d69f38
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-09-26 11:43:02 +0200

    Fix

--- gozba-na-klik/src/App.jsx
+++ gozba-na-klik/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Welcome from "./pages/Welcome";
 import Admin from "./pages/Admin";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Admin/>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/admin" element={<Admin/>} />
+    </Routes>
   );
 }
 
