037db13d8aaf953ab4bc30bf6000d2eaa87f9323
Branko Kovacevic <branko.kovacevic09@gmail.com>
2025-09-28 20:13:55 +0200

    sitne promene u import u App

--- gozba-na-klik/src/pages/Login.jsx
+++ gozba-na-klik/src/pages/Login.jsx
@@ -5,31 +5,39 @@ import { useForm } from "react-hook-form";
 import { handleLogin } from "../api/authService";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    return (
-        <form onSubmit={handleSubmit((formData) => handleLogin(navigate, formData))}>
-            <h2>LOGIN</h2>
+  return (
+    <form
+      onSubmit={handleSubmit((formData) => handleLogin(navigate, formData))}
+    >
+      <h2>LOGIN</h2>
 
-            <input
-                {...register("email", { required: "Obavezno polje" })}
-                placeholder="Email"
-                type="email"
-            />
-            {errors.email && <p>{errors.email.message}</p>}
+      <input
+        {...register("email", { required: "Obavezno polje" })}
+        placeholder="Email"
+        type="email"
+      />
+      {errors.email && <p>{errors.email.message}</p>}
 
-            <input
-                {...register("password", { required: "Obavezno polje" })}
-                placeholder="Password"
-                type="password"
-            />
-            {errors.password && <p>{errors.password.message}</p>}
+      <input
+        {...register("password", { required: "Obavezno polje" })}
+        placeholder="Password"
+        type="password"
+      />
+      {errors.password && <p>{errors.password.message}</p>}
 
-            <button type="submit">Login</button>
-            <button type="button" onClick={() => navigate("/")}>Back</button>
-        </form>
-    );
-}
+      <button type="submit">Login</button>
+      <button type="button" onClick={() => navigate("/")}>
+        Back
+      </button>
+    </form>
+  );
+};
 
 export default Login;
